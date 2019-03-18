4530b46739d75ca23ca74aa0bd23033f9fbede99
Sooty Forge <jetttay@hotmail.com>
2019-03-18 22:47:46 +1100

    JT 003 - Animations, Shooting
    Tweaked animations, and got it shooting.
    Shooting needs work.

---
+++ Assets/MVP/Scripts/AI/AI_WeaponTEST.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GameSystems;
+
+public class AI_WeaponTEST : Weapon
+{
+    public AI_ScoutDrone contact;
+
+    void Update()
+    {
+        Attack();
+    }
+
+    public override void Attack()
+    {
+        if (contact.fov.visibleTargets.Count > 0)
+        {
+            StartCoroutine("Shoot");
+            print("Firing.");
+        }
+        else
+        {
+            StopCoroutine("Shoot");
+            print("Target lost.");
+        }
+
+    }
+
+    IEnumerator Shoot()
+    {
+        while (true)
+        {
+            GameObject clone = Instantiate(projectile, spawnPoint.position, spawnPoint.rotation);
+            Bullet newBullet = clone.GetComponent<Bullet>();
+
+            newBullet.Fire(spawnPoint.transform.forward);
+            yield return new WaitForSeconds(Random.Range(0.1f, 0.5f));
+        }
+    }
+}
