d85b404e6265bb4c04f43ca901d961177097d00f
yirulz <36685131+yirulz@users.noreply.github.com>
2019-03-19 15:32:17 +1100

    Door working with animation

---
+++ Assets/Yi/DoorTrigger.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorTrigger : MonoBehaviour
+{
+    [Header("Animation")]
+    public Animator anim;
+
+    [Header("Variables")]
+    public GameObject enemy;
+
+    void Start()
+    {
+        
+    }
+    void Update()
+    {
+        if(enemy.transform.childCount <= 0)
+        {
+            anim.SetBool("Exit", true);
+        }
+    }
+
+
+    //When player enters triggerbox
+    void OnTriggerEnter(Collider other)
+    {
+        //Only trigger is tag is player
+        if (other.gameObject.CompareTag("Player"))
+        {
+            Debug.Log("Collision detected");
+            //Set the trigger for the animator
+            anim.SetTrigger("Enter");
+        }
+    }
+}
