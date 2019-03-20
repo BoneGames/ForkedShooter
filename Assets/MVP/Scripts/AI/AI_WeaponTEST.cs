74a82c15d93ee415f81b2261f5377ffa7d27daf1
SootyForge <42379545+SootyForge@users.noreply.github.com>
2019-03-20 13:20:33 +1100

    JT 004 - Shooting Patch
    Stopped it from shooting like mad. It was just running 'StartCoroutine("")' in Update() that was the problem.

--- Assets/MVP/Scripts/AI/AI_WeaponTEST.cs
+++ Assets/MVP/Scripts/AI/AI_WeaponTEST.cs
@@ -5,37 +5,50 @@ using GameSystems;
 
 public class AI_WeaponTEST : Weapon
 {
+    #region Variable
+    // Check in AI_ScoutDrone.cs for visibleTargets.
     public AI_ScoutDrone contact;
+    #endregion
 
-    void Update()
+    #region Functions 'n' Methods
+    // Where we initialize / Start things.
+    void Start()
     {
-        Attack();
+        // Grab 'contact's component, and start Coroutine.
+        contact = gameObject.GetComponent<AI_ScoutDrone>();
+        StartCoroutine("Shoot");
     }
 
+    // Where we define shooting.
     public override void Attack()
     {
+        // If we've found a target...
         if (contact.fov.visibleTargets.Count > 0)
         {
-            StartCoroutine("Shoot");
+            // Fire bullets at it.
+            GameObject clone = Instantiate(projectile, spawnPoint.position, spawnPoint.rotation);
+            Bullet newBullet = clone.GetComponent<Bullet>();
+
+            newBullet.Fire(spawnPoint.transform.forward);
             print("Firing.");
         }
+        // Otherwise stop firing.
         else
         {
-            StopCoroutine("Shoot");
             print("Target lost.");
         }
 
     }
 
+    // Where we run Attack.
     IEnumerator Shoot()
     {
+        // While the Coroutine is running...
         while (true)
         {
-            GameObject clone = Instantiate(projectile, spawnPoint.position, spawnPoint.rotation);
-            Bullet newBullet = clone.GetComponent<Bullet>();
-
-            newBullet.Fire(spawnPoint.transform.forward);
+            Attack();
             yield return new WaitForSeconds(Random.Range(0.1f, 0.5f));
         }
-    }
+    } 
+    #endregion
 }
