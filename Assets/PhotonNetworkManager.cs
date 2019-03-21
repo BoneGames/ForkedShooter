60bf1e272541a569a565bd4db9dabd371f04f161
BoneGames <44772942+BoneGames@users.noreply.github.com>
2019-03-21 23:12:56 +1100

    Photon setUp

---
+++ Assets/PhotonNetworkManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PhotonNetworkManager : Photon.MonoBehaviour {
+	[SerializeField] private Text connectText;
+	[SerializeField] private GameObject player;
+	[SerializeField] private GameObject lobbyCamera;
+	[SerializeField] private Transform spawnPoint;
+	// Use this for initialization
+	void Start () {
+		PhotonNetwork.ConnectUsingSettings("0.1");
+	}
+
+	public virtual void OnJoinedLobby()
+	{
+		Debug.Log("We have now joined lobby");
+		PhotonNetwork.JoinOrCreateRoom("testLobby", null, null);
+	}
+
+	public virtual void OnJoinedRoom()
+	{
+		// spawn in player prefab
+		PhotonNetwork.Instantiate(player.name, spawnPoint.position, spawnPoint.rotation, 0);
+		// disable lobby cam
+		lobbyCamera.SetActive(false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		connectText.text = PhotonNetwork.connectionStateDetailed.ToString();
+	}
+}
---
+++ Assets/PlayerNetwork.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerNetwork : MonoBehaviour {
+
+	[SerializeField] private GameObject playerCam;
+	[SerializeField] private MonoBehaviour[] playerControlScripts;
+	private PhotonView photonView;
+
+	private void Start()
+	{
+		photonView = GetComponent<PhotonView>();
+		Initialise();
+	}
+
+	private void Initialise()
+	{
+		if(!photonView.isMine)
+		{
+			// disable other player's FP cam
+			playerCam.SetActive(false);
+			// disable other player's control scripts
+			foreach(MonoBehaviour m in playerControlScripts)
+			{
+				m.enabled = false;
+			}
+		}
+	}
+}
