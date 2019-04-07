e0c62dc93caa6f893dfc47e97b96f4a1ad1c6bc9
Sooty Forge <jetttay@hotmail.com>
2019-04-07 17:02:22 +1000

    JT 022 - Master Push 002
    Added weapon accuracy, particles to enemy weapons and bullet impacts.

--- Assets/MVP/Scripts/AI/AI_Weapon.cs
+++ Assets/MVP/Scripts/AI/AI_Weapon.cs
@@ -7,8 +7,14 @@ public class AI_Weapon : Weapon
 {
     #region Variable
     // Check in AI_ScoutDrone.cs for visibleTargets.
-    public BehaviourAI contact;
-    public float reloadTime;
+    [Header("AI Weapon Variables")]
+    public BehaviourAI contact; // Contact with the BehaviourAI conditions.
+    public GameObject hitParticle;
+
+    [Header("Burst Fire")]
+    public int burstCount; // Number of shots fired per burst (1 = semi-automatic).
+    public float burstDelay; // Time between each shot fired in a burst.
+    public float reloadTime; // Self explanatory.
     #endregion
 
     #region Functions 'n' Methods
@@ -20,55 +26,9 @@ public class AI_Weapon : Weapon
         StartCoroutine("Shoot");
     }
 
-    // Where we define shooting.
-    public override void Attack()
+    private void Update()
     {
-        //Debug.Log("Attack");
-        // If there is a player in our line of sight, and we still have ammo to work with...
-        if (contact.fov.visibleTargets.Count > 0  && currentMag > 0)
-        {
-            
-            RaycastHit hit;
-            Ray ray = new Ray(spawnPoint.position, spawnPoint.transform.forward);
-            
-            if (Physics.Raycast(ray, out hit))
-            {
-                /// For reference to see where bullets hit;
-                ///GameObject bullet = Instantiate(GameObject.CreatePrimitive(PrimitiveType.Cube), hit.point, Quaternion.identity);
-                ///bullet.GetComponent<Renderer>().material.color = Color.red;
-                ///bullet.transform.localScale = new Vector3(.15f, .15f, .15f);
-
-                if (GameManager.isOnline)
-                {
-                    if (hit.collider.CompareTag("Player"))
-                    {
-                        hit.transform.GetComponent<PhotonView>().RPC("ChangeHealth", PhotonTargets.All, damage);
-                    }
-                }
-                else
-                {
-                    if (hit.collider.tag == "Player")
-                    {
-                        hit.transform.GetComponent<Health>().ChangeHealth(damage);
-                        print("AI hit an enemy");
-                    }
-                }
-                /// // Fire bullets at it.
-                /// GameObject clone = Instantiate(projectile, spawnPoint.position, spawnPoint.rotation);
-                /// Bullet newBullet = clone.GetComponent<Bullet>();
-                /// 
-                /// newBullet.Fire(spawnPoint.transform.forward);
-                /// newBullet.sourceAgent = this.gameObject;
-                /// print("Firing.");
-                currentMag--;
-            }
-        }
-        // If we run out of ammo, start reloading and stop shooting.
-        if (currentMag == 0)
-        {
-            StartCoroutine("StartReload", reloadTime);
-            StopCoroutine("Shoot");
-        }
+        Debug.DrawRay(spawnPoint.position, spawnPoint.transform.forward);
     }
 
     // Where we run Attack() multiple times.
@@ -84,18 +44,18 @@ public class AI_Weapon : Weapon
         }
     }
 
-    
     // Where we run BurstFire().
     IEnumerator Shoot()
     {
         while (true)
         {
+            Debug.Log("shootroutine");
             // Run BurstFire every 0.5 ↔ 1.0 seconds.
 
             //This form of StartCoroutine doesn't use the string name to run it
             //This makes it more consistent with other functions, and simplifies 
             //passing in more paramaters 
-            StartCoroutine(BurstFire(3, .1f));
+            StartCoroutine(BurstFire(burstCount, burstDelay));
             yield return new WaitForSeconds(Random.Range(0.5f, 1f));
         }
     }
@@ -112,6 +72,76 @@ public class AI_Weapon : Weapon
             StartCoroutine("Shoot");
             StopCoroutine("StartReload");
         }
+    }
+
+    // Where we define shooting.
+    public override void Attack()
+    {
+        Debug.Log("Attack");
+        // If there is a player in our line of sight, and we still have ammo to work with...
+        if (contact.fov.visibleTargets.Count > 0 && currentMag > 0)
+        {
+
+            RaycastHit hit;
+            Ray ray = new Ray(spawnPoint.position, spawnPoint.transform.forward);
+
+            SpawnMuzzleFlash();
+
+            Vector3 direction = transform.forward;
+
+            direction.x += Random.Range(-accuracy, accuracy);
+            direction.y += Random.Range(-accuracy, accuracy);
+
+            if (Physics.Raycast(ray.origin, direction, out hit))
+            {
+                /// For reference to see where bullets hit;
+                ///GameObject bullet = Instantiate(GameObject.CreatePrimitive(PrimitiveType.Cube), hit.point, Quaternion.identity);
+                ///bullet.GetComponent<Renderer>().material.color = Color.red;
+                ///bullet.transform.localScale = new Vector3(.15f, .15f, .15f);
+
+                SpawnHitParticle(hit.point);
+
+                if (GameManager.isOnline)
+                {
+                    if (hit.collider.CompareTag("Player"))
+                    {
+                        hit.transform.GetComponent<PhotonView>().RPC("ChangeHealth", PhotonTargets.All, damage);
+                    }
+                }
+                else
+                {
+                    print("I'm firing!");
+                    Debug.DrawRay(spawnPoint.position, spawnPoint.forward, Color.red);
+
+                    if (hit.collider.tag == "Player")
+                    {
+                        hit.transform.GetComponent<Health>().ChangeHealth(damage);
+                        //print("I hit an enemy");
+                    }
+                }
+                /// // Fire bullets at it.
+                /// GameObject clone = Instantiate(projectile, spawnPoint.position, spawnPoint.rotation);
+                /// Bullet newBullet = clone.GetComponent<Bullet>();
+                /// 
+                /// newBullet.Fire(spawnPoint.transform.forward);
+                /// newBullet.sourceAgent = this.gameObject;
+                /// print("Firing.");
+                currentMag--;
+                Debug.Log(currentMag);
+            }
+        }
+        // If we run out of ammo, start reloading and stop shooting.
+        if (currentMag == 0)
+        {
+            StartCoroutine("StartReload", reloadTime);
+            StopCoroutine("Shoot");
+        }
+    }
+
+    void SpawnHitParticle(Vector3 hit)
+    {
+        GameObject _flash = Instantiate(hitParticle, hit, Quaternion.identity);
+        Destroy(_flash, 3);
     }
     #endregion
 }
